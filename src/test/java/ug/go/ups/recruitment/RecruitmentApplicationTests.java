46a5daf82c320876df54ae2a590be16ac2146639
raduga256 <56635875+raduga256@users.noreply.github.com>
2020-08-28 11:20:56 +0300

    Initial commit

---
+++ src/test/java/ug/go/ups/recruitment/RecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ug.go.ups.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

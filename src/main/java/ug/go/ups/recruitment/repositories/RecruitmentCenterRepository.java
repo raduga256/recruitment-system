a70c450e2eae96a4d9f180a46aa0aa25ceabb888
raduga256 <56635875+raduga256@users.noreply.github.com>
2020-08-30 13:42:42 +0300

    Created CRUD Repo for Entity Recruitment Center. closes #11

---
+++ src/main/java/ug/go/ups/recruitment/repositories/RecruitmentCenterRepository.java
@@ -0,0 +1,7 @@
+package ug.go.ups.recruitment.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import ug.go.ups.recruitment.domain.RecruitmentCenter;
+
+public interface RecruitmentCenterRepository extends CrudRepository<RecruitmentCenter, Long> {
+}

88c6b68d8c4980d66cd291f51cf8cc5c26b668be
raduga256 <56635875+raduga256@users.noreply.github.com>
2020-08-30 13:44:54 +0300

    Created CRUD Repo for Entity District. closes #12

---
+++ src/main/java/ug/go/ups/recruitment/repositories/DistrictRepository.java
@@ -0,0 +1,7 @@
+package ug.go.ups.recruitment.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import ug.go.ups.recruitment.domain.District;
+
+public interface DistrictRepository extends CrudRepository<District, Long> {
+}
